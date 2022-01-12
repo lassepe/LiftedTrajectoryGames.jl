499b6287efdd230f449b25d7e1323b2b50e2f7cb
lassepe <lasse.peters@mailbox.org>
2022-01-12 20:49:35 +0100

    Un-deprecate OnlineOptimizationActionGenerator

--- src/trajectory_parameter_generators.jl
+++ src/trajectory_parameter_generators.jl
@@ -1,3 +1,5 @@
+#== NNActionGenerator ==#
+
 struct NNActionGenerator{M,O}
     model::M
     optimizer::O
@@ -39,3 +41,29 @@ function update_parameters!(g, ∇)
     θ = Flux.params(g)
     Optimise.update!(g.optimizer, θ, ∇)
 end
+
+#== OnlineOptimizationActionGenerator ==#
+
+struct OnlineOptimizationActionGenerator{T<:AbstractMatrix,O}
+    params::T
+    optimizer::O
+end
+
+function OnlineOptimizationActionGenerator(;
+    n_actions,
+    n_params,
+    params_abs_max,
+    learning_rate,
+    rng,
+)
+    params = (rand(rng, n_params, n_actions) .- 0.5) .* (2params_abs_max)
+    optimizer = Optimise.Descent(learning_rate)
+
+    OnlineOptimizationActionGenerator(params, params_abs_max, optimizer)
+end
+@functor OnlineOptimizationActionGenerator (params,)
+
+function (g::OnlineOptimizationActionGenerator)(_)
+    collect(eachcol(g.params))
+end
+
