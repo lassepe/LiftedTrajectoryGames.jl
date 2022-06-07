bdac62b903cf9d27fd3bfd1220b4f9e6a5cc199d
lassepe <lasse.peters@mailbox.org>
2022-06-07 12:21:28 +0200

    [wip] streamline visualization interface
    There is now a single `visualize!` function

--- src/visualization.jl
+++ src/visualization.jl
@@ -1,16 +1,14 @@
 """
 Visualize a strategy `γ` on a makie canvas using the base color `color`.
-
-TODO: could make use of `Makie.@recipe` here.
 """
-function TrajectoryGamesBase.visualize_strategy!(
-    axis,
-    γ::Makie.Observable{<:LiftedTrajectoryStrategy},
-    color;
+function TrajectoryGamesBase.visualize!(
+    canvas,
+    γ::Makie.Observable{<:LiftedTrajectoryStrategy};
+    color = :black,
     weight_offset = 0.0,
 )
     trajectory_colors = Makie.@lift([(color, w + weight_offset) for w in $γ.weights])
-    Makie.series!(axis, γ; color = trajectory_colors)
+    Makie.series!(canvas, γ; color = trajectory_colors)
 end
 
 function Makie.convert_arguments(::Type{<:Makie.Series}, γ::LiftedTrajectoryStrategy)
